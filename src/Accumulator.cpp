7aefe50182868301f20a2f6fa4003646b0cd3d34
Gavin Rough <gavin.rough@gobble.com>
2021-12-17 14:17:40 -0500

    Add second accumulator

--- src/Accumulator.cpp
+++ src/Accumulator.cpp
@@ -6,83 +6,116 @@ struct Accumulator : Module {
     PARAMS_LEN
   };
   enum InputId {
-    RATE_INPUT,
-    RESET_INPUT,
+    RATE_1_INPUT,
+    RESET_1_INPUT,
+    RATE_2_INPUT,
+    RESET_2_INPUT,
     INPUTS_LEN
   };
   enum OutputId {
-    SUM_OUTPUT,
+    SUM_1_OUTPUT,
+    SUM_2_OUTPUT,
     OUTPUTS_LEN
   };
   enum LightId {
     LIGHTS_LEN
   };
 
-  float sums[16] = {0.0f};
-  float channels = 0;
-  dsp::BooleanTrigger resetTrigger[16];
+  int rateI[2];
+  int resetI[2];
+  int sumO[2];
+
+  float sums[2][16] = {{0.0f}};
+  float channels[2] = {0};
+  dsp::BooleanTrigger resetTrigger[2][16];
 
   Accumulator() {
     config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-    configInput(RATE_INPUT, "Growth rate");
-    configInput(RESET_INPUT, "Reset");
-    configOutput(SUM_OUTPUT, "Total");
+    configInput(RATE_1_INPUT, "Growth rate");
+    configInput(RATE_2_INPUT, "Growth rate");
+    configInput(RESET_1_INPUT, "Reset");
+    configInput(RESET_2_INPUT, "Reset");
+    configOutput(SUM_1_OUTPUT, "Total");
+    configOutput(SUM_2_OUTPUT, "Total");
+
+    rateI[0] = RATE_1_INPUT;
+    rateI[1] = RATE_2_INPUT;
+    resetI[0] = RESET_1_INPUT;
+    resetI[1] = RESET_2_INPUT;
+    sumO[0] = SUM_1_OUTPUT;
+    sumO[1] = SUM_2_OUTPUT;
   }
 
   json_t *dataToJson() override {
-    json_t *root = json_object();
-    json_t *sumsJ = json_array();
-    for (size_t c = 0; c < 16; c++) {
-      json_array_append_new(sumsJ, json_real(sums[c]));
+    json_t *rootJ = json_object();
+    json_t *configsJ = json_array();
+    for (size_t i = 0; i < 2; i++) {
+      json_t *sumJ = json_object();
+      json_object_set_new(sumJ, "channels", json_integer(channels[i]));
+      json_array_append_new(configsJ, sumJ);
+      json_t *sumsJ = json_array();
+      for (size_t c = 0; c < channels[i]; c++) {
+        json_array_append_new(sumsJ, json_real(sums[i][c]));
+      }
+      json_object_set_new(sumJ, "sums", sumsJ);
     }
-    json_object_set_new(root, "sums", sumsJ);
-    json_object_set_new(root, "channels", json_integer(channels));
-    return root;
+    json_object_set_new(rootJ, "accumulator", configsJ);
+    return rootJ;
   }
 
   void dataFromJson(json_t *root) override {
-    json_t *sumsJ = json_object_get(root, "sums");
-    if (sumsJ) {
+    json_t *configsJ = json_object_get(root, "accumulator");
+    if (configsJ) {
       size_t i;
-      json_t *sumJ;
-      json_array_foreach(sumsJ, i, sumJ) {
-        float sum = json_number_value(sumJ);
-        sums[i] = sum;
+      json_t *configJ;
+      json_array_foreach(configsJ, i, configJ) {
+        json_t *channelsJ = json_object_get(configJ, "channels");
+        channels[i] = json_number_value(channelsJ);
+        json_t *sumsJ = json_object_get(configJ, "sums");
+        if (sumsJ) {
+          size_t c;
+          json_t *sumJ;
+          json_array_foreach(sumsJ, c, sumJ) {
+            sums[i][c] = json_number_value(sumJ);
+          }
+        }
       }
-    }
-    json_t *channelsJ = json_object_get(root, "channels");
-    if (channelsJ) {
-      channels = json_number_value(channelsJ);
     }
   }
 
   void process(const ProcessArgs &args) override {
-    if (inputs[RATE_INPUT].getChannels() > channels) {
-      channels = inputs[RATE_INPUT].getChannels();
-    }
-
-    outputs[SUM_OUTPUT].setChannels(channels);
-
-    for (int c = 0; c < channels; c++) {
-      sums[c] += inputs[RATE_INPUT].getVoltage(c) * args.sampleTime;
-      outputs[SUM_OUTPUT].setVoltage(sums[c], c);
-    }
-
-    if (inputs[RESET_INPUT].isMonophonic()) {
-      if (resetTrigger[0].process(inputs[RESET_INPUT].getVoltage() > 0.0f)) {
-        for (int c = 0; c < 16; c++) {
-          sums[c] = 0.0f;
-        }
-        channels = 0;
+    for (int i = 0; i < 2; i++) {
+      if (inputs[rateI[i]].getChannels() > channels[i]) {
+        channels[i] = inputs[rateI[i]].getChannels();
       }
-    }
 
-    if (inputs[RESET_INPUT].isPolyphonic()) {
-      for (int c = 0; c < inputs[RESET_INPUT].getChannels(); c++) {
-        if (resetTrigger[c].process(inputs[RESET_INPUT].getVoltage(c) > 0.0f)) {
-          sums[c] = 0.0f;
-          if (c == channels - 1) {
-            channels--;
+      if (channels[i] == 0) {
+        return;
+      }
+
+      outputs[sumO[i]].setChannels(channels[i]);
+
+      for (int c = 0; c < channels[i]; c++) {
+        sums[i][c] += inputs[rateI[i]].getVoltage(c) * args.sampleTime;
+        outputs[sumO[i]].setVoltage(sums[i][c], c);
+      }
+
+      if (inputs[resetI[i]].isMonophonic()) {
+        if (resetTrigger[i][0].process(inputs[resetI[i]].getVoltage() > 0.0f)) {
+          for (int c = 0; c < 16; c++) {
+            sums[i][c] = 0.0f;
+          }
+          channels[i] = 0;
+        }
+      }
+
+      if (inputs[resetI[i]].isPolyphonic()) {
+        for (int c = 0; c < inputs[resetI[i]].getChannels(); c++) {
+          if (resetTrigger[i][c].process(inputs[resetI[i]].getVoltage(c) > 0.0f)) {
+            sums[i][c] = 0.0f;
+            if (c == channels[i] - 1) {
+              channels[i]--;
+            }
           }
         }
       }
@@ -101,10 +134,13 @@ struct AccumulatorWidget : ModuleWidget {
     addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
     addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-    addInput(createInputCentered<V1Port>(mm2px(Vec(7.62, 75.357)), module, Accumulator::RATE_INPUT));
-    addInput(createInputCentered<V1Port>(mm2px(Vec(7.62, 93.857)), module, Accumulator::RESET_INPUT));
+    addInput(createInputCentered<V1Port>(mm2px(Vec(7.62, 15.357)), module, Accumulator::RATE_1_INPUT));
+    addInput(createInputCentered<V1Port>(mm2px(Vec(7.62, 33.857)), module, Accumulator::RESET_1_INPUT));
+    addInput(createInputCentered<V1Port>(mm2px(Vec(7.62, 75.357)), module, Accumulator::RATE_2_INPUT));
+    addInput(createInputCentered<V1Port>(mm2px(Vec(7.62, 93.857)), module, Accumulator::RESET_2_INPUT));
 
-    addOutput(createOutputCentered<V1Port>(mm2px(Vec(7.62, 112.357)), module, Accumulator::SUM_OUTPUT));
+    addOutput(createOutputCentered<V1Port>(mm2px(Vec(7.62, 52.357)), module, Accumulator::SUM_1_OUTPUT));
+    addOutput(createOutputCentered<V1Port>(mm2px(Vec(7.62, 112.357)), module, Accumulator::SUM_2_OUTPUT));
   }
 };
 
